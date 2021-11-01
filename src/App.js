1758bd4871e9f6fe3f1397043a3d7b768d84dece
Adam Victor <abenjamin1313@gmail.com>
2021-11-01 17:05:27 -0400

    Completed UI for Login and Sign up pages.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() =>  import('./pages/login'));
+const Signup = lazy(() =>  import('./pages/sign-up'));
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
         </Switch>
       </Suspense>
     </Router>
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -3,9 +3,12 @@ import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 
-export default function Login() {
+export default function Signup() {
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
+
+    const [username, setUsername] = useState('');
+    const [fullname, setFullname] = useState('');
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -13,21 +16,20 @@ export default function Login() {
     const [error, setError] = useState('');
     const isInvalid = password === '' || email === '';
 
-    const handLogin = async (event) => {
+    const handSignup = async (event) => {
         event.preventDefault();
 
-        try {
-            await firebase.auth().signInWithEmailAndPassword(email, password);
-            history.push(ROUTES.DASHBOARD);
-        } catch (error) {
-            setEmail('');
-            setPassword('');
-            setError(error.message);
-        }
+        const usernameExists = await doesUsernameExist(username);
+
+        // try {
+           
+        // } catch (error) {
+            
+        // }
     };
 
     useEffect(() => {
-        document.title = 'Login - Career Day App';
+        document.title = 'Sign up - Career Day App';
     }, []);
 
     return (
@@ -42,8 +44,24 @@ export default function Login() {
                     </h1>
                     {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-                    <form onSubmit={handLogin} method="POST">
-                        <input aria-label="Enter your email address"
+                    <form onSubmit={handSignup} method="POST">
+                        <input aria-label="Enter your username."
+                        type="text" 
+                        placeholder="Username"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+                        border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setUsername(target.value)}
+                        value={username}
+                        />
+                        <input aria-label="Enter your full name."
+                        type="text" 
+                        placeholder="Full name"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
+                        border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setFullname(target.value)}
+                        value={fullname}
+                        />
+                        <input aria-label="Enter your email address."
                         type="text" 
                         placeholder="Email Address"
                         className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
@@ -51,7 +69,7 @@ export default function Login() {
                         onChange={({ target }) => setEmail(target.value)}
                         value={email}
                         />
-                        <input aria-label="Enter your password address"
+                        <input aria-label="Enter your password address."
                         type="password" 
                         placeholder="Password"
                         className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border 
@@ -66,13 +84,13 @@ export default function Login() {
                             className={
                                 `bg-blue-medium text-white w-full rounded h-8 font-bold
                             ${isInvalid && 'opacity-50'}`}>
-                        Log In
+                        Sign Up
                         </button>
                     </form>
                     <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border bored-gray-primary">
-                        <p className="text-sm">Don't have an ancount?{``}
-                            <Link to={ROUTES.SIGN_UP}  className="font-bold text-blue-medium ml-2">
-                                Sign Up
+                        <p className="text-sm">Have an ancount?{``}
+                            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium ml-2">
+                                Login
                             </Link>
                         </p>
                     </div>
